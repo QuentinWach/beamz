ea1ef026f9d4b37ace8a8c40f1c83cf2f3d6c627
Quentin Wach <quentin.wach@googlemail.com>
2025-10-19 20:00:16 +0200

    .

--- examples/0_dipole.py
+++ examples/0_dipole.py
@@ -9,7 +9,7 @@ DX, DT = calc_optimal_fdtd_params(WL, max(N_CORE, N_CLAD), dims=2, safety_factor
 # Create the design
 design = Design(8*µm, 8*µm, material=Material(N_CLAD**2))
 design += Rectangle(width=4*µm, height=4*µm, material=Material(N_CORE**2))
-design.show()
+
 time_steps = np.arange(0, TIME, DT)
 signal = ramped_cosine(time_steps, amplitude=1.0, frequency=LIGHT_SPEED/WL, ramp_duration=3*WL/LIGHT_SPEED, t_max=TIME/2)
 source = GaussianSource(position=(4*µm, 5*µm), width=WL/6, signal=signal)
