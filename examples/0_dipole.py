3071d9670a3c4d6ceeabe92a73a635c85b1bd5bb
Quentin Wach <quentin.wach@googlemail.com>
2025-10-19 20:00:02 +0200

    .

--- examples/0_dipole.py
+++ examples/0_dipole.py
@@ -10,13 +10,9 @@ DX, DT = calc_optimal_fdtd_params(WL, max(N_CORE, N_CLAD), dims=2, safety_factor
 design = Design(8*µm, 8*µm, material=Material(N_CLAD**2))
 design += Rectangle(width=4*µm, height=4*µm, material=Material(N_CORE**2))
 design.show()
-
-grid = design.rasterize(resolution=0.1*µm)
-
 time_steps = np.arange(0, TIME, DT)
 signal = ramped_cosine(time_steps, amplitude=1.0, frequency=LIGHT_SPEED/WL, ramp_duration=3*WL/LIGHT_SPEED, t_max=TIME/2)
 source = GaussianSource(position=(4*µm, 5*µm), width=WL/6, signal=signal)
 
 sim = Simulation(design=design, devices=[source], time=time_steps, resolution=DX)
-# Run with live Ez field animation (set animate_live=None to disable)
 sim.run(animate_live="Ez", animation_interval=1)
