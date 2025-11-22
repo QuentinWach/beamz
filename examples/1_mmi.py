1db13c69e87a9f59e2c0131ffd2f14f88cecca11
Quentin Wach <quentin.wach@googlemail.com>
2025-11-22 17:54:14 +0100

    .

--- examples/1_mmi.py
+++ examples/1_mmi.py
@@ -8,7 +8,7 @@ TIME = 40*WL/LIGHT_SPEED # total simulation duration
 N_CORE, N_CLAD = 2.04, 1.444 # Si3N4, SiO2
 WG_W = 0.565*µm # width of the waveguide
 H, W, OFFSET = 3.5*µm, 9*µm, 1.05*µm # height, length, offset of the MMI
-DX, DT = calc_optimal_fdtd_params(WL, max(N_CORE, N_CLAD), dims=2, safety_factor=0.999, points_per_wavelength=20) 
+DX, DT = calc_optimal_fdtd_params(WL, max(N_CORE, N_CLAD), dims=2, safety_factor=0.999, points_per_wavelength=12) 
 
 # Design the MMI with input and output waveguides
 design = Design(width=X, height=Y, material=Material(N_CLAD**2))
@@ -26,7 +26,7 @@ grid.show(field="permittivity")
 time_steps = np.arange(0, TIME, DT)
 signal = ramped_cosine(time_steps, amplitude=0.1, frequency=LIGHT_SPEED/WL, ramp_duration=WL*6/LIGHT_SPEED, t_max=TIME/2)
 # Prefer TE polarization and restrict transverse width to single-mode core to avoid exciting higher-order lobes
-source = ModeSource(grid=grid, center=(3*µm, Y/2), width=WG_W, wavelength=WL, pol="te", signal=signal, direction="+x")
+source = ModeSource(grid=grid, center=(3*µm, Y/2), width=WG_W, wavelength=WL, pol="tm", signal=signal, direction="+x")
 
 # Run the simulation and show results
 sim = Simulation(design=design, devices=[source], boundaries=[PML(edges='all', thickness=1.2*WL)], time=time_steps, resolution=DX)
