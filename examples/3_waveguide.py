3943d902a45ce678fd8763f26863ce9d9b2d98ab
Quentin Wach <quentin.wach@googlemail.com>
2025-11-22 21:31:18 +0100

    Fixed Waveguide Example.

--- examples/3_waveguide.py
+++ examples/3_waveguide.py
@@ -1,6 +1,6 @@
 from beamz import *
 import numpy as np
-from beamz.helpers import calc_optimal_fdtd_params
+from beamz import calc_optimal_fdtd_params
 
 WL = 1.55*µm
 TIME = 90*WL/LIGHT_SPEED
@@ -9,7 +9,7 @@ WG_WIDTH = 0.565*µm
 DX, DT = calc_optimal_fdtd_params(WL, max(N_CORE, N_CLAD), safety_factor=0.999, points_per_wavelength=20)
 
 # Create the design
-design = Design(width=18*µm, height=7*µm, material=Material(N_CLAD**2), pml_size=WL)
+design = Design(width=18*µm, height=7*µm, material=Material(N_CLAD**2))
 design += Rectangle(position=(0,3.5*µm-WG_WIDTH/2), width=18*µm, height=WG_WIDTH, material=Material(N_CORE**2))
 design.show()
 
@@ -30,16 +30,19 @@ signal = ramped_cosine(
 source = ModeSource(
     grid=grid,
     center=(4 * µm, design.height / 2),
-    width=1.5 * µm,
+    width=WG_WIDTH * 1.2, # Slightly wider than waveguide to capture mode tails, but not so wide to hit PML/boundaries
     wavelength=WL,
     pol="tm",
     signal=signal,
     direction="+x",
 )
-# Visualize the solved modes
-source.show(component="Ez")
 
 # Run the simulation
-sim = FDTD(design=grid, devices=[source], time=time_steps)
-sim.run(live=True, save_memory_mode=True, accumulate_power=True)
-sim.plot_power(db_colorbar=True)
+sim = Simulation(
+    design=design, 
+    devices=[source], 
+    boundaries=[PML(edges='all', thickness=1.2*WL)],
+    time=time_steps,
+    resolution=DX
+)
+sim.run(animate_live="Ez", animation_interval=20)
