a96a0c04d0abf94131aadfed1494dd28ec162bde
Quentin Wach <quentin.wach@googlemail.com>
2026-01-02 20:26:11 +0100

    Added PML to 3D example.

--- examples/6_3d_waveguide.py
+++ examples/6_3d_waveguide.py
@@ -2,6 +2,7 @@ import numpy as np
 from beamz import Design, Rectangle, Material, ModeSource, Monitor, Simulation, ramped_cosine
 from beamz.const import µm, LIGHT_SPEED
 from beamz.visual.helpers import calc_optimal_fdtd_params
+from beamz import *
 
 # Units and constants
 WL = 1.55 * µm  # Wavelength
@@ -13,15 +14,15 @@ N_CORE = 3.48  # Silicon
 # Calculate optimal grid parameters
 # 3D simulations can be memory intensive, so we use a slightly lower resolution (8 points/WL)
 DX, DT = calc_optimal_fdtd_params(WL, N_CORE, dims=3, safety_factor=0.999,
-    points_per_wavelength=14, width=4*µm, height=4*µm, depth=2*µm)
+    points_per_wavelength=14, width=6.5*µm, height=6.5*µm, depth=4*µm)
 
 # 1. Create the Design
 # A 10µm long waveguide along X, 4µm wide, 2µm thick
-design = Design(width=4*µm, height=4*µm, depth=2*µm, material=Material(N_AIR**2))
-design += Rectangle(position=(0, 0, 0), width=4*µm, height=4*µm, depth=1*µm, material=Material(N_CLAD**2))
+design = Design(width=6.5*µm, height=6.5*µm, depth=4*µm, material=Material(N_AIR**2))
+design += Rectangle(position=(0, 0, 0), width=6.5*µm, height=6.5*µm, depth=2*µm, material=Material(N_CLAD**2))
 waveguide = Rectangle(
-    position=(0, 1.75*µm, 1*µm), 
-    width=4*µm, 
+    position=(0, 3*µm, 2*µm), 
+    width=6.5*µm, 
     height=0.5*µm, 
     depth=0.22*µm, 
     material=Material(N_CORE**2)
@@ -44,7 +45,7 @@ grid = design.rasterize(resolution=DX)
 # Using 0.8µm (slightly larger than waveguide height 0.5µm) to capture mode field
 source = ModeSource(
     grid=grid,
-    center=(1.0*µm, 2.0*µm, 1.11*µm),  # Z at waveguide center
+    center=(1*WL, 3.25*µm, 2.11*µm),  # Z at waveguide center
     width=0.8*µm,  # Closer to waveguide height (0.5µm) to better capture mode
     wavelength=WL,
     pol="te",
@@ -57,16 +58,17 @@ source = ModeSource(
 # 4. Add Monitors
 # XY plane monitor in the middle of the waveguide thickness
 monitor_xy = Monitor(
-    start=(0, 0, 1.11*µm),
-    size=(4*µm, 4*µm),
+    start=(0, 0, 2.11*µm),
+    size=(6.5*µm, 6.5*µm),
     plane_normal="z",
     name="xy_plane"
 )
-design += monitor_xy
-design.show()
+#design += monitor_xy
+#design.show()
 
 # 5. Run the Simulation
-sim = Simulation(design=design, devices=[source, monitor_xy], time=time_steps, resolution=DX)
+sim = Simulation(design=design, devices=[source, monitor_xy], 
+boundaries=[PML(edges='all', thickness=0.75*WL)], time=time_steps, resolution=DX)
 
 # Run with live animation of the Ez field on the XY monitor
 sim.run(animate_live="Ez", animation_interval=5, clean_visualization=True)
