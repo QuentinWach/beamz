8a8a4e543b15c9be9c8bfbac33bf7fb2e3b19ef3
Quentin Wach <quentin.wach@googlemail.com>
2026-03-14 19:11:43 +0100

    Show the widget in the jupyter notebooks!

--- beamz/visual/scene/_browser.py
+++ beamz/visual/scene/_browser.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import html
 import tempfile
 import webbrowser
 from pathlib import Path
@@ -21,3 +22,18 @@ def open_in_browser(
     if open_browser:
         webbrowser.open(url)
     return url
+
+
+def inline_iframe_html(
+    scene: SceneSpec | dict[str, Any],
+    *,
+    height: int = 640,
+) -> str:
+    scene_spec = scene if isinstance(scene, SceneSpec) else scene_from_dict(scene)
+    srcdoc = html.escape(browser_html(scene_spec), quote=True)
+    return (
+        f'<iframe srcdoc="{srcdoc}" '
+        'style="width: 100%; min-height: 480px; '
+        f'height: {int(height)}px; border: 0; border-radius: 16px;" '
+        'sandbox="allow-scripts allow-same-origin"></iframe>'
+    )
